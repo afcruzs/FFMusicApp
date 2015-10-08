f555c51762f7ecc6afcbb23efd65959aca964902
Milder Hernandez Cagua <milderhc@gmail.com>
2015-10-08 16:58:49 -0500

    Adding mimimum changes about Facebook login

--- app/src/main/java/ffmusic/com/ffmusicapp/controller/LoginActivity.java
+++ app/src/main/java/ffmusic/com/ffmusicapp/controller/LoginActivity.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -18,22 +19,32 @@ import ffmusic.com.ffmusicapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginButton loginButton;
+    private LoginButton loginFacebookButton;
     private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Check if a person is already logged
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
         setContentView(R.layout.activity_login);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginFacebookButton = (LoginButton)findViewById(R.id.login_button);
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginFacebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                // It's necessary to check if a person is already registered in FFMusic
 
+                // Here, some method from User (model) is called to register a new user
+                // User.createNewUser(loginResult);
+
+
+                //After checking MainActivity is started
+                startMainActivity();
             }
 
             @Override
@@ -49,10 +60,13 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void startMainActivity ( ) {
+        startActivity(new Intent(this, FFMusicMainActivity.class));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
-        startActivity(new Intent(this, FFMusicMainActivity.class));
     }
 
     @Override
@@ -75,5 +89,17 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume ( ) {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
     }
 }
