cd1732656fdea44d2724a481111f1438de04c0ca
Milder Hernandez Cagua <milderhc@gmail.com>
2015-10-12 17:10:20 -0500

    Adding FloatingActionButton to create new rooms

--- app/src/main/java/ffmusic/com/ffmusicapp/controller/RoomsFragment.java
+++ app/src/main/java/ffmusic/com/ffmusicapp/controller/RoomsFragment.java
@@ -5,10 +5,12 @@ package ffmusic.com.ffmusicapp.controller;
  */
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import ffmusic.com.ffmusicapp.R;
 
@@ -16,6 +18,15 @@ public class RoomsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rooms, container, false);
+        View view = inflater.inflate(R.layout.fragment_rooms, container, false);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "CreateNewUser", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
